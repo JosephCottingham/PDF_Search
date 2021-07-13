6c5638b7bf8a31b4fa07a5cd16c47962e3f19853
josephcottingham <joseph.h.cottingham@gmail.com>
2021-07-13 14:16:08 -0400

    .env for flask change

--- search_interface/app.py
+++ search_interface/app.py
@@ -1,7 +1,28 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+import psycopg2
+import psycopg2.extras
+import traceback
+
+load_dotenv()
+
+host = os.environ.get("POSTGRES_HOSTNAME")
+user = os.environ.get("POSTGRES_USER")
+password = os.environ.get("POSTGRES_PASSWORD")
+db = os.environ.get("POSTGRES_DATABASE")
+
+conn = None
+
+def get_connection():
+    global conn
+    if conn is None or conn.closed == 1:
+        print("Creating a new connection")
+        conn = psycopg2.connect(host=host, database=db, user=user, password=password)
+    return conn
 
 app = Flask(__name__)
-
 @app.route("/")
 def search():
+    conn = get_connection()
     return "<p>Hello, World!</p>"
