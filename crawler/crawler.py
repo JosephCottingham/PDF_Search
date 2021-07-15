234c15509ffb56c3c3790b9ac42fe104ae8c8cff
Brenner Haverlock <80720533+officialbrenner@users.noreply.github.com>
2021-07-15 09:44:53 -0700

    update for demo

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,46 +1,13 @@
+import re
+from urllib import urlopen
+
+html = urlopen("http://www.nycgo.com/venues/thalia-restaurant#menu")
+html_doc = html.read()
+
+match = re.search(b'\"(.*?\.pdf)\"', html_doc)
+pdf_url = "http://www.nycgo.com" + match.group(1).decode('utf8')
 
 
-import requests
-from urllib.parse import urlparse, urljoin
-from bs4 import BeautifulSoup
-import colorama
-
-
-base_path = os.environ.get("PDF_BASE_PATH") or 'pdf/'
-
-
-colorama.init()
-GREEN = colorama.Fore.GREEN
-GRAY = colorama.Fore.LIGHTBLACK_EX
-RESET = colorama.Fore.RESET
-YELLOW = colorama.Fore.YELLOW
-
-# initialize the set of links (unique links)
-internal_urls = set()
-external_urls = set()
-
-
-def is_valid(url):
-    """
-    Checks whether `url` is a valid URL.
-    """
-    parsed = urlparse(url)
-    return bool(parsed.netloc) and bool(parsed.scheme)
-
-
-
-def get_all_website_links(url):
-    """
-    Returns all URLs that is found on `url` in which it belongs to the same website
-    """
-    # all URLs of `url`
-    urls = set()
-    # domain name of the URL without the protocol
-    domain_name = urlparse(url).netloc
-    soup = BeautifulSoup(requests.get(url).content, "html.parser")
-
-for a_tag in soup.findAll("a"):
-        href = a_tag.attrs.get("href")
-        if href == "" or href is None:
-            # href empty tag
-            continue
+text_file = open("pdf.txt", "w")
+n = text_file.write(pdf_url)
+text_file.close()
