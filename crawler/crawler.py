b2264173c9c90027c10b4a6a8d3a76ac1592ffc3
Brenner Haverlock <80720533+officialbrenner@users.noreply.github.com>
2021-07-15 10:03:30 -0700

    updated from local

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,13 +1,57 @@
-import re
-from urllib import urlopen
 
-html = urlopen("http://www.nycgo.com/venues/thalia-restaurant#menu")
-html_doc = html.read()
+# for get the pdf files or url
+import requests
+  
+# for tree traversal scraping in webpage
+from bs4 import BeautifulSoup
+  
+# for input and output operations
+import io
+  
+# For getting information about the pdfs
+from PyPDF2 import PdfFileReader
 
-match = re.search(b'\"(.*?\.pdf)\"', html_doc)
-pdf_url = "http://www.nycgo.com" + match.group(1).decode('utf8')
 
+# website to scrap
+url = "https://www.geeksforgeeks.org/how-to-extract-pdf-tables-in-python/"
+  
+# get the url from requests get method
+read = requests.get(url)
+  
+# full html content 
+html_content = read.content
+  
+# Parse the html content 
+soup = BeautifulSoup(html_content, "html.parser")
 
-text_file = open("pdfs/pdf.txt", "w")
-n = text_file.write(pdf_url)
+# created an empty list for putting the pdfs 
+list_of_pdf = set()
+  
+# accessed the fist p tag in the html 
+l = soup.find('p') 
+  
+# accessed all the anchors tag from given p tag
+p = l.find_all('a') 
+  
+# iterate through p for getting all the href links
+for link in p: 
+      
+    # original html links
+    print("links: ", link.get('href'))
+    print("\n")
+      
+    # converting the extention from .html to .pdf
+    pdf_link = (link.get('href')[:-5]) + ".pdf"
+      
+    # converted to .pdf
+    print("converted pdf links: ", pdf_link)
+    print("\n")
+      
+    # added all the pdf links to set
+    list_of_pdf.add(pdf_link)
+
+s = str(list_of_pdf)
+
+text_file = open("pdf.txt", "w")
+n = text_file.write(s)
 text_file.close()
