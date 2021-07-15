dd2402cf9af6892f4c4e244bedcfe07a07b58d21
Brenner Haverlock <80720533+officialbrenner@users.noreply.github.com>
2021-07-15 09:51:39 -0700

    Update crawler.py

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -8,6 +8,6 @@ match = re.search(b'\"(.*?\.pdf)\"', html_doc)
 pdf_url = "http://www.nycgo.com" + match.group(1).decode('utf8')
 
 
-text_file = open("pdf/pdf.txt", "w")
+text_file = open("pdfs/pdf.txt", "w")
 n = text_file.write(pdf_url)
 text_file.close()
