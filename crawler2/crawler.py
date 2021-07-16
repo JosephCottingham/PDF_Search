678bccbfffe6aa0167e44ba6c8a3a59406f8f745
josephcottingham <joseph.h.cottingham@gmail.com>
2021-07-16 17:33:45 -0400

    parse google search urls with only one hard code k

--- crawler2/crawler.py
+++ crawler2/crawler.py
@@ -11,16 +11,21 @@ from requests import get
 def crawl():
     # for ss in wn.synsets('car'):
     #     for word in ss.lemma_names():
-    word = 'car'
-    # raw = get(f"https://www.google.com/search?q=filetype%3Apdf&q={word}").text
-    raw = get(f"http://www.pdfsearchengine.net/searchresult.html?cx=partner-pub-9634067433254658%3A9653363797&cof=FORID%3A10&ie=UTF-8&q=filetype%3Apdf+{word}&qfront={word}&siteurl=http%3A%2F%2Fwww.pdfsearchengine.net%2F&algorithm=filetype%3Apdf+").text
-    page = fromstring(raw)
-    print(raw)
-    for result in page.cssselect("a"):
-        url = result.get("href")
-        print(url)
-        # if '/url?q=' in url:
-        #     url = url[7:]
+    word = '{word}'
+    raw = get(f"https://www.google.com/search?q=filetype%3Apdf&q={word}").text
+    # raw = get(f"http://www.pdfsearchengine.net/searchresult.html?cx=partner-pub-9634067433254658%3A9653363797&cof=FORID%3A10&ie=UTF-8&q=filetype%3Apdf+{word}&qfront={word}&siteurl=http%3A%2F%2Fwww.pdfsearchengine.net%2F&algorithm=filetype%3Apdf+").text
+    file = open('sit', 'w')
+    file.write(raw)
+    file.close()
+    index= 0
+    print(len(raw))
+    while index < len(raw):
+        index = raw.find('/url?q', index)
+        if index == -1:
+            break
+        index = index + 7
+        end_index = raw.find('&amp;', index)
+        url = raw[index:end_index]
         if ('http' in url and check_pdf(url)):
             write_url_to_text_file(url)
 
