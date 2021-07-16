0d1efc8b9227cd8713f92369b4e11363fa4961a2
josephcottingham <joseph.h.cottingham@gmail.com>
2021-07-16 19:15:27 -0400

    stop exit

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -9,26 +9,29 @@ base_path = os.environ.get("PDF_BASE_PATH") or 'pdf/'
 
 def crawl():
     while True:
-        print('Sleep')
-        time.sleep(random.randint(0,15))
-        print('Wake')
-        word = random.choice(words.words())
-        print(word)
-        raw = get(f"https://www.google.com/search?q=filetype%3Apdf&q={word}").text
-        file = open('sit', 'w')
-        file.write(raw)
-        file.close()
-        index= 0
-        print(len(raw))
-        while index < len(raw):
-            index = raw.find('/url?q', index)
-            if index == -1:
-                break
-            index = index + 7
-            end_index = raw.find('&amp;', index)
-            url = raw[index:end_index]
-            if ('http' in url and check_pdf(url)):
-                write_url_to_text_file(url)
+        try:
+            print('Sleep')
+            time.sleep(random.randint(0,15))
+            print('Wake')
+            word = random.choice(words.words())
+            print(word)
+            raw = get(f"https://www.google.com/search?q=filetype%3Apdf&q={word}").text
+            file = open('sit', 'w')
+            file.write(raw)
+            file.close()
+            index= 0
+            print(len(raw))
+            while index < len(raw):
+                index = raw.find('/url?q', index)
+                if index == -1:
+                    break
+                index = index + 7
+                end_index = raw.find('&amp;', index)
+                url = raw[index:end_index]
+                if ('http' in url and check_pdf(url)):
+                    write_url_to_text_file(url)
+        except:
+            continue
 
 def check_pdf(url):
     r = get(url, allow_redirects=True)
