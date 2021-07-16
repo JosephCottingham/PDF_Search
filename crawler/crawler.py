800fe2f3e93da1a2af6926eeb7f6984f4316a86c
Brenner Haverlock <80720533+officialbrenner@users.noreply.github.com>
2021-07-15 18:04:17 -0700

    update crawler

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,69 +1,85 @@
-
-# for get the pdf files or url
 import requests
-  
-# for tree traversal scraping in webpage
+# Library for parsing HTML
 from bs4 import BeautifulSoup
-  
-# for input and output operations
-import io
+base_url = 'https://dumps.wikimedia.org/enwiki/'
+index = requests.get(base_url).text
+soup_index = BeautifulSoup(index, 'html.parser')
+# Find the links on the page
+dumps = [a['href'] for a in soup_index.find_all('a') if 
+         a.has_attr('href')]
 
-import os
-  
-# For getting information about the pdfs
-from PyPDF2 import PdfFileReader
+# print(dumps)
 
 
-# website to scrap
-url = "https://effiesplace.net/our-menus/view-pdf-menus/"
+dump_url = base_url + '20210520/'
+
+# print(dump_url)
+# Retrieve the html
+dump_html = requests.get(dump_url).text
+# Convert to a soup
+soup_dump = BeautifulSoup(dump_html, 'html.parser')
+# Find list elements with the class file
+soup_dump = soup_dump.find_all('li', {'class': 'file'})[:10]
+
+# print(soup_dump)
   
 # get the url from requests get method
-read = requests.get(url)
+read = requests.get(dump_url)
   
 # full html content 
 html_content = read.content
   
 # Parse the html content 
 soup = BeautifulSoup(html_content, "html.parser")
+  # created an empty list for putting the pdfs
+list_of_links = set()
 
-# created an empty list for putting the pdfs 
-list_of_pdf = set()
-  
-# accessed the fist p tag in the html 
-l = soup.find('p') 
-  
+# accessed the fist p tag in the html
+l = soup.find('li')
+
+# print(l)
+
 # accessed all the anchors tag from given p tag
-p = l.find_all('a') 
-  
+p = l.find_all('a')
+
+# print(p)
+
 # iterate through p for getting all the href links
-for link in p: 
-      
-    # original html links
-    print("links: ", link.get('href'))
-    print("\n")
-      
-    # converting the extention from .html to .pdf
-    pdf_link = (link.get('href')[:-5]) + ".pdf"
-      
-    # converted to .pdf
-    print("converted pdf links: ", pdf_link)
-    print("\n")
-      
-    # added all the pdf links to set
-    list_of_pdf.add(pdf_link)
+for link in p:
+	
+	# original html links
+	# print("links: ", link.get('href'))
+	# print("\n")
+	
+	# converting the extention 
+	bz2_link = (link.get('href')[:-5]) + ".bz2"
+	
+	# converted to .pdf
+	print("converted bz2 links: ", bz2_link)
+	print("\n")
+	bz2_link = "https://dumps.wikimedia.org" + bz2_link
+	# added all the pdf links to set
+	list_of_links.add(bz2_link)
 
 
-save_path = 'crawler/pdfs/'
-file_name = "pdf.txt"
-
-completeName = os.path.join(save_path, file_name)
-print(completeName)
-
-s = str(list_of_pdf)
-s = s.strip('{}')
-s = s.strip(" ' ' ")
+print(list_of_links)
 
 
-text_file = open(completeName, "w")
-n = text_file.write(s)
-text_file.close()
+
+import tensorflow as tf
+from tensorflow import keras
+import subprocess
+
+# from tensorflow.python.keras.utils import get_file
+saved_file_path = tf.keras.utils.get_file(
+    fname = 'enwiki-20210520-pages-articles-multistream.xml.bz2', origin = 'https://dumps.wikimedia.org/enwiki/20210520/enwiki-20210520-pages-articles-multistream.xml.bz2')
+
+print(saved_file_path)
+lst = []
+# data_path = "~/.keras/datasets/enwiki-20210520-pages-articles-multistream.xml.bz2"
+# Iterate through compressed file one line at a time
+for line in subprocess.Popen(['bzcat'], 
+                              stdin = open(saved_file_path), 
+                              stdout = subprocess.PIPE).stdout:
+                              lst.append((line))
+print(lst)
