6f610c199d2c26379a0f68be83948a395f20725d
Brenner Haverlock <80720533+officialbrenner@users.noreply.github.com>
2021-07-16 10:55:18 -0700

    updated steady crawler

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,85 +1,107 @@
+# for get the pdf files or url
 import requests
-# Library for parsing HTML
+import uuid
+
+# for tree traversal scraping in webpage
 from bs4 import BeautifulSoup
-base_url = 'https://dumps.wikimedia.org/enwiki/'
-index = requests.get(base_url).text
-soup_index = BeautifulSoup(index, 'html.parser')
-# Find the links on the page
-dumps = [a['href'] for a in soup_index.find_all('a') if 
-         a.has_attr('href')]
 
-# print(dumps)
+# for input and output operations
+import io
+import os
+
+# For getting information about the pdfs
+from PyPDF2 import PdfFileReader
 
 
-dump_url = base_url + '20210520/'
+# website to scrap
+url = "https://en.wikipedia.org/wiki/PDF"
 
-# print(dump_url)
-# Retrieve the html
-dump_html = requests.get(dump_url).text
-# Convert to a soup
-soup_dump = BeautifulSoup(dump_html, 'html.parser')
-# Find list elements with the class file
-soup_dump = soup_dump.find_all('li', {'class': 'file'})[:10]
-
-# print(soup_dump)
-  
 # get the url from requests get method
-read = requests.get(dump_url)
-  
-# full html content 
+read = requests.get(url)
+
+# full html content
 html_content = read.content
-  
-# Parse the html content 
+
+# Parse the html content
 soup = BeautifulSoup(html_content, "html.parser")
   # created an empty list for putting the pdfs
-list_of_links = set()
+list_of_pdf = set()
 
 # accessed the fist p tag in the html
-l = soup.find('li')
-
-# print(l)
+l = soup.find('p')
 
 # accessed all the anchors tag from given p tag
 p = l.find_all('a')
 
-# print(p)
+# uid
+unique_filename = str(uuid.uuid4())
+
+import uuid
+import random
+random = str(uuid.UUID(int=random.getrandbits(128)))
 
 # iterate through p for getting all the href links
 for link in p:
-	
+
 	# original html links
-	# print("links: ", link.get('href'))
-	# print("\n")
-	
-	# converting the extention 
-	bz2_link = (link.get('href')[:-5]) + ".bz2"
-	
-	# converted to .pdf
-	print("converted bz2 links: ", bz2_link)
+	print("links: ", link.get('href'))
 	print("\n")
-	bz2_link = "https://dumps.wikimedia.org" + bz2_link
+
+	# converting the extention from .html to .pdf
+	pdf_link = (link.get('href')[:-5]) + ".pdf"
+	 
+	# converted to .pdf
+	print("converted pdf links: ", pdf_link)
+	print("\n")
+	pdf_link = "https://en.wikipedia.org/wiki/PDF" + pdf_link
+	save_path = 'crawler/pdfs/'
+	file_name =  str(random) + ".txt"
+	completeName = os.path.join(save_path, file_name)
+	text_file = open(completeName, "w")
+	text_file.write(pdf_link)
+	text_file.close()
+
+
+
 	# added all the pdf links to set
-	list_of_links.add(bz2_link)
-
-
-print(list_of_links)
+	# list_of_pdf.add(pdf_link) 
 
 
 
-import tensorflow as tf
-from tensorflow import keras
-import subprocess
+# for line in list_of_pdf:
+	 
 
-# from tensorflow.python.keras.utils import get_file
-saved_file_path = tf.keras.utils.get_file(
-    fname = 'enwiki-20210520-pages-articles-multistream.xml.bz2', origin = 'https://dumps.wikimedia.org/enwiki/20210520/enwiki-20210520-pages-articles-multistream.xml.bz2')
 
-print(saved_file_path)
-lst = []
-# data_path = "~/.keras/datasets/enwiki-20210520-pages-articles-multistream.xml.bz2"
-# Iterate through compressed file one line at a time
-for line in subprocess.Popen(['bzcat'], 
-                              stdin = open(saved_file_path), 
-                              stdout = subprocess.PIPE).stdout:
-                              lst.append((line))
-print(lst)
+# save_path = 'crawler/pdfs/'
+# file_name =  "pdf.txt"
+
+# completeName = os.path.join(save_path, file_name)
+# text_file = open(completeName, "w")
+
+# s = str(list_of_pdf)
+# s = s.strip('{}')
+# n = text_file.write(s)
+
+# text_file.write("\n")
+# text_file.close()
+# # s = s.strip(" ' ' ")
+
+
+
+
+# with open('crawler/pdfs/pdf.txt','r') as pdf:
+
+# 	for element in pdf:
+# 		print(element)
+		# n = text_file.write(element)
+		# text_file.write("\n")
+		# text_file.close()
+
+
+# with open('crawler/pdfs/pdf.txt','r') as pdf:
+# 	for line in pdf:
+# 		s.strip().split(',')
+# 		print(line)
+
+        # with open(line.rstrip() + '.txt','w') as new_file:
+        #     new_file.write(line)
